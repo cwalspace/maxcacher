19fb9b6372c02d94c1f2ddd1c2edf48e5e2e128f
Por Corp <porcorpwise@gmail.com>
2020-02-19 23:25:55 +0500

    runtime fix / DRY

--- src/Driver/Runtime.php
+++ src/Driver/Runtime.php
@@ -2,40 +2,55 @@
 
 namespace cwalspace\MaxCacher\Driver;
 
-use cwalspace\MaxCacher\Config;
-
 class Runtime
 {
     protected $config;
     private $map;
 
-    public function __construct(Config $config)
+    public function __construct(\cwalspace\MaxCacher\Config $config)
     {
         $this->config = $config;
     }
 
-    public function set($key, $value, $ttl)
+    public function set($key, $value, $ttl = null)
     {
-        if ($ttl <= time() && $ttl !== null) {
-            unset($this->map[md5($key)]);
-        } 
+        if ($ttl < time()) {
+            throw new Exception("error: past TTL");
+        }
 
         if ($ttl === null) {
             $ttl = time() + $this->config->default_ttl;
         }
 
         $this->map[md5($key)] = ['val' => $value, 'ttl' => $ttl];
-
         return true;
     }
 
     public function get($key)
     {
-        if (time() < $this->map[md5($key)]['ttl']) {
-            return $this->map[md5($key)]['val'];
-        } else {
-            unset($this->map[md5($key)]);
+        $hash = md5($key);
+
+        if (!isset($this->map[$hash])) {
             return null;
         }
+
+        if (time() < $this->map[$hash]['ttl']) {
+            return $this->map[$hash]['val'];
+        }
+
+        unset($this->map[$hash]);
+        return null;
+    }
+
+    public function del($key)
+    {
+        $hash = md5($key);
+
+        if (isset($this->map[$hash])) {
+            unset($this->map[$hash]);
+            return true;
+        } 
+
+        throw new Exception('error: key was not found');
     }
 }
