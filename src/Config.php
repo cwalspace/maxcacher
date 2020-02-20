45defa95f1948c8737589fd6743548268d513f54
Por Corp <porcorpwise@gmail.com>
2020-02-20 05:35:12 +0500

    setters for config

--- src/Config.php
+++ src/Config.php
@@ -4,16 +4,37 @@ namespace cwalspace\MaxCacher;
 
 class Config
 {
-    private $config = [];
+    private $config = ['driver' => 'runtime', 'default_ttl' => 3600, 'path' => __DIR__, 'prefix' => 'rs'];
+    public  $driver = null;
     
     public function __construct($config)
     {
-        $this->config = $config;
+        $this->config = array_merge($this->config, $config);
     }
 
     public function set($key, $value = null)
     {
         $this->config[$key] = $value;
+    }
+
+    public function getDefaultTtl()
+    {
+        return isset($this->config['default_ttl']) ? $this->config['default_ttl'] : 0;
+    }
+
+    public function getPath()
+    {
+        return isset($this->config['path']) ? $this->config['path'] : __DIR__;
+    }
+
+    public function getDriver() 
+    {
+        return isset($this->config['driver']) ? $this->config['driver'] : null;
+    }
+
+    public function getPrefix() 
+    {
+        return isset($this->config['prefix']) ? $this->config['prefix'] : '';
     }
 
     public function get($key)
